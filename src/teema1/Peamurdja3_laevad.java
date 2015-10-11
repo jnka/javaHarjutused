9f93602e9e6b4bcf26b799b19291bcde2607d9cc
unknown <janikaa@Playtech-L1280.EE.playtech.corp>
2015-10-11 16:33:33 +0300

    Praktikum3

--- src/teema1/Peamurdja3_laevad.java
+++ src/teema1/Peamurdja3_laevad.java
@@ -16,4 +16,33 @@ import java.util.Scanner;
  *    nii vertikaalselt kui ka horisontaalselt?
  */
 public class Peamurdja3_laevad {
+    public static void main(String[] args) {
+        int[] laud = {1, 1, 1, 0, 0, 1, 0, 0, 1}; //[] näitab numbrite massiiivi, laua lugemine: 0, 1, 2, 3, jne.
+        System.out.println(Arrays.toString(laud));//võtab laua, teeb massivist teksti ja prindib välja selle ühe reana;
+
+        Scanner kasutaja = new Scanner(System.in); //kasutajalt info küsimine
+        while (gameover(laud)) {
+            System.out.println("Sisesta üks number");//prindib kasutajale küsimuse välja
+            int sisestus = kasutaja.nextInt();//kasutaja sisestus (numbriline)
+            int hit = laud[sisestus]; //
+            if (hit == 0) {
+                System.out.println("Möödas!");
+            } else if (hit == 1) {
+                System.out.println("Pihtas!");
+                laud[sisestus] = 2;
+            } else if (hit == 2) {
+                System.out.println("Juba lasid selle põhja, proovi uuesti!");
+            }
+        }
+        System.out.println("Jee, võitsid!!!");
+    }
+    public static boolean gameover(int[] laud){//Kontrollib, kas 1 on veel lauas ja annab vastuseks true või false. Kui enam ühtegi 1-te pole, siis lõpetab mängu.
+        for (int i = 0; i < laud.length; i++) {//int i = 0 - näitab ära alguse; i < laud.length - kaua kestab mäng,määrab ära laua pikkuse,  i++ - lisab alati juurde kuni laua lõpuni.
+            if (laud[i] == 1) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
